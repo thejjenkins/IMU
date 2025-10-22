9d505b688523a43ef91f19245a9e330be5292b15
James Jenkins <james.jenkins745@protonmail.com>
2025-10-22 17:06:53 -0400

    changed baud rate to 115200 and added ability to sample for a number of seconds

--- gatherUART.py
+++ gatherUART.py
@@ -1,20 +1,23 @@
 import serial
 import csv
+import time
 
+path = 'IMU/imu_data.csv'
 try:
     ser = serial.Serial(
         port='COM4',
-        baudrate=9600,
+        baudrate=115200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1  # Timeout in seconds for read operations
     )
     print(f"Serial port {ser.name} opened successfully.")
-    with open('imu_data.csv', mode='w', newline='') as file:
+    with open(path, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Accel_X', 'Accel_Y', 'Accel_Z', 'Gyro_X', 'Gyro_Y', 'Gyro_Z'])  # Header
-        while True:
+        start_time = time.time()
+        while (time.time() - start_time) < 100:
             ser.reset_input_buffer()
             data = ser.readline().decode('utf-8').strip()  # Read a line and decode to string
             if data:
